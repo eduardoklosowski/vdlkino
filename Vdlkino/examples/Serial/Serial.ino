84c5835cbdd336c81952a11f2fcb0596f79732ed
Eduardo Klosowski <eduardo_klosowski@yahoo.com>
2014-03-30 14:43:29 -0300

    Example to use Serial

---
+++ Vdlkino/examples/Serial/Serial.ino
@@ -0,0 +1,18 @@
+/*
+    Copyright (c) 2014 Eduardo Augusto Klosowski
+    License: MIT (see LICENSE for details)
+*/
+
+#include <Vdlkino.h>
+#include <VdlkinoSerial.h>
+
+VdlkinoSerial vdlkino(14, 6, &Serial);
+
+void setup() {
+    Serial.begin(9600);
+}
+
+void loop() {
+    vdlkino.run();
+    delay(1000);
+}
